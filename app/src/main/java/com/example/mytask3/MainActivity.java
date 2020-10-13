cc44c7ecab5ba7ecd7762099a4b3b331f7961b54
Chapaev Anton <virsus2009@gmail.com>
2020-10-13 23:23:26 +0700

    Добавил проверку в onCreate
    *при повороте экрана
    *убрал поток
    *убрал излишние методы

--- app/src/main/java/com/example/mytask3/MainActivity.java
+++ app/src/main/java/com/example/mytask3/MainActivity.java
@@ -18,28 +18,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ColdStart(3);
+        Log.d("TEST", "onCreate: ok ");
+        if (savedInstanceState == null) {
+            setColdStart(3);
+        } else Log.d("TEST", "onCreate: rotation ");
     }
 
 
-    private void ColdStart(final int setIterationStart) {
-        Thread coldStart = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                final int numberStart = getNumberStart();
-                setNumberStart(numberStart);
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        showToast(numberStart, setIterationStart);
-                    }
-                });
-                Log.d("TEST", "onCreate:" + numberStart);
-            }
-        });
-        coldStart.start();
+    private void setColdStart(int setIterationStart) {
+        int numberStart = getNumberStart();
+        setNumberStart(numberStart);
+        showToast(numberStart, setIterationStart);
+        Log.d("TEST", "onCreate:" + numberStart);
     }
+
+    private void setNumberStart(int number) {
+        coldStart = getSharedPreferences(START, MODE_PRIVATE);
+        SharedPreferences.Editor writeMode = coldStart.edit();
+        writeMode.putInt(COUNT, ++number);
+        writeMode.commit();
+    }
+
+    private int getNumberStart() {
+        coldStart = getSharedPreferences(START, MODE_PRIVATE);
+        return coldStart.getInt(COUNT, 0);
+    }
+
 
     private void showToast(int number, int iteration) {
         if (number == iteration - 1) {
@@ -48,20 +52,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void setNumberStart(int number) {
-        number++;
-        setShared();
-        SharedPreferences.Editor writeMode = coldStart.edit();
-        writeMode.putInt(COUNT, number);
-        writeMode.commit();
-    }
-
-    private int getNumberStart() {
-        setShared();
-        return coldStart.getInt(COUNT, 0);
-    }
-
-    private void setShared() {
-        coldStart = getSharedPreferences(START, MODE_PRIVATE);
-    }
 }
