4f555bd7f58115da0d4e564430045b3e2bfc600f
ChapaevAnton <virsus2009@gmail.com>
2020-10-14 12:12:56 +0700

    Переработал метод
    *убрал лишний поток
    *убрал лишние методы, сделал все в один

--- app/src/main/java/com/example/mytask3/MainActivity.java
+++ app/src/main/java/com/example/mytask3/MainActivity.java
@@ -7,49 +7,47 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
 
-    private final String START = "start";
-    private final String COUNT = "count";
-    private SharedPreferences coldStart;
+public class MainActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setShowToastColdStart(3, "Холодный старт", savedInstanceState);
+    }
+
+
+
+    /**
+     * @param setIterationStart - на каком холодном запуске показывать Toast
+     * @param msg - произвольное сообщение Toast
+     * @param getColdStart - состояние текущего экземляра Activity
+     */
+
+    private void setShowToastColdStart(final int setIterationStart, final CharSequence msg, Bundle getColdStart) {
         Log.d("TEST", "onCreate: ok ");
-        if (savedInstanceState == null) {
-            setColdStart(3);
+        if (getColdStart == null) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    final String START = "start";
+                    final String COUNT = "count";
+                    SharedPreferences coldStart = getSharedPreferences(START, MODE_PRIVATE);
+                    int numberStart = coldStart.getInt(COUNT, 1);
+                    Log.d("TEST", "onCreate:" + numberStart);
+                    if (numberStart == setIterationStart) {
+                        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+                        Log.d("TEST", "showToast: show");
+                    }
+                    SharedPreferences.Editor writeMode = coldStart.edit();
+                    writeMode.putInt(COUNT, ++numberStart);
+                    writeMode.commit();
+                }
+            });
         } else Log.d("TEST", "onCreate: rotation ");
-    }
-
-
-    private void setColdStart(int setIterationStart) {
-        int numberStart = getNumberStart();
-        setNumberStart(numberStart);
-        showToast(numberStart, setIterationStart);
-        Log.d("TEST", "onCreate:" + numberStart);
-    }
-
-    private void setNumberStart(int number) {
-        coldStart = getSharedPreferences(START, MODE_PRIVATE);
-        SharedPreferences.Editor writeMode = coldStart.edit();
-        writeMode.putInt(COUNT, ++number);
-        writeMode.commit();
-    }
-
-    private int getNumberStart() {
-        coldStart = getSharedPreferences(START, MODE_PRIVATE);
-        return coldStart.getInt(COUNT, 0);
-    }
-
-
-    private void showToast(int number, int iteration) {
-        if (number == iteration - 1) {
-            Toast.makeText(MainActivity.this, "Холодный старт!!!", Toast.LENGTH_LONG).show();
-            Log.d("TEST", "showToast: show");
-        }
     }
 
 }
